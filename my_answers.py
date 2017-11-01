5bc813054d96cf0f9e6e9498dfe7e4dd1f5d2d02
Michael Cervantes <mcervantes@tropo.com>
2017-10-31 23:45:02 -0700

    implement my answers

--- my_answers.py
+++ my_answers.py
@@ -10,8 +10,8 @@ import keras
 # and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_series(series, window_size):
     # containers for input/output pairs
-    X = []
-    y = []
+    X = [ series[idx - window_size:idx] for idx in range(window_size,len(series)) ]
+    y = series[window_size:]
 
     # reshape each 
     X = np.asarray(X)
@@ -23,24 +23,32 @@ def window_transform_series(series, window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
+    return Sequential([
+        LSTM(5, input_shape=(window_size,1)),
+        Dense(1)
+    ])
 
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
-
-    return text
+    text = list(text)
+    for idx, c in enumerate(text):
+        text[idx] = (c if ((c >= 'a' and c <= 'z') or c in punctuation) else ' ')
+    return ''.join(text)
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_text(text, window_size, step_size):
     # containers for input/output pairs
-    inputs = []
-    outputs = []
+    inputs = [ text[idx - window_size:idx] for idx in range(window_size,len(text),step_size) ]
+    outputs = [ text[idx] for idx in range(window_size,len(text),step_size) ]
 
     return inputs,outputs
 
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
-    pass
+    return Sequential([
+        LSTM(200, input_shape=(window_size,num_chars)),
+        Dense(num_chars, activation='softmax')
+    ])
